8dfb98fe5e08e7ab236a5d70f20edebef2cb1033
TehBrian <tehbrian@proton.me>
2026-05-19 13:20:36 -0400

    set up Vite proxy for dev DB

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
   },
 })
